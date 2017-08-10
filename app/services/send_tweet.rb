8e17d5d24f98f82a48990df1b25c6af1f2e0aab7
gusholguin <augustin.holguin@gmail.com>
2017-08-10 15:16:33 +0200

    we got some layout boys

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -1,12 +1,27 @@
+#!/usr/bin/env ruby
+require "twitter"
+
 class SendTweet
 
-  def initialize
+  def initialize (tweet)
+    @tweet = tweet
   end
 
+end
+
+private
+
   def log_in_to_twitter
+      @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "95062M0UShvrETyxqpnZavPoe"
+      config.consumer_secret     = "supOIufJlhD2slrXZsnzfhzWXEO2zdldyS5ghhem4PQf2a9vOT"
+      config.access_token        = "885126648730988545-4H6IXL3mq4eUz2ngxiAmKnYjD24X4IO"
+      config.access_token_secret = "O8UuvngeouTq8JQ4qdK5gwNSe149M54fu4ed1P0R94DLA"
+    end
   end
 
   def send_tweet
+    @client.update(@tweet)
   end
 
 end
